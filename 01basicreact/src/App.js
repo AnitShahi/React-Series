23977085cd2acfc755825c1af98b69d4f69ba945
Anit Shahi <anitrajshahi11@gmail.com>
2024-08-08 21:38:00 +0545

    vite with react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1> chai aru react | Anit Shahi </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>chai aru react with vite | AS</h1>
+  )
+}
+
+export default App
