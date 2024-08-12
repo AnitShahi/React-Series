24c6a61ef890657ae26717a6060b78178e096118
Anit Shahi <anitrajshahi11@gmail.com>
2024-08-12 22:05:55 +0545

    custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,10 +2,12 @@ import Chai from "./Chai"
 
 function App() {
 
+  const username = "Anit Shahi"
+
   return (
     <>
     <Chai />
-    <h1>chai aru react with vite | AS</h1>
+    <h1>chai aru react with vite | {username} </h1>
     <p>test para</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+    <h1>Custom App | AS</h1>
+  </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'click me to visit google'
+
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>vist google</a>
+)
+
+const anotheruser = "chai aru react"
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target:'_blank'},
+  "click me to vist google",
+  anotheruser,
+
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <App />
-  </StrictMode>,
+  
 )
